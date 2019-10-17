fb5b96044480b7603af80755a0d7397be2d8bfc5
Leah Kurtz <lfkurtz7@gmail.com>
2019-10-17 14:18:27 -0400

    used .push to get letters guessed to appear on screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,7 @@ console.log(computerChoice);
  
 
 var userChoiceText = document.getElementById("userchoice-text");
-var computerChoiceText = document.getElementById("computerchoice-text");
+var computerChoiceText = document.getElementById("computerchoice-text"); // do i keep the same name for this variable?//
 var winsText = document.getElementById("wins-span");
 var lossesText = document.getElementById("losses-span");
 var guessesLeftText = document.getElementById("guessesLeft-span");
@@ -18,29 +18,35 @@ var guessesSoFarText = document.getElementById("guessesSoFar-span");
 winsText.textContent= wins;
 lossesText.textContent= losses;
 guessesLeftText.textContent= guessesLeft;
-guessesSoFarText.textContent= guessesSoFar;
+guessesSoFarText.textContent= guessesSoFar; 
 
 
 document.onkeyup = function (event) {
     var userChoice = event.key;
-    var computerChoice = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+    var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)]; // showing up as undefined//
 
-    if (userChoice === computerChoice) {
+    if (userChoice === computerGuess) {
         wins++
+
         
+        winsText.textContent= wins;  
         
-        // show wins increase, show wins by setting .text line 18
 
 
     } else {
-        losses++ 
-        guessesSoFarText.push(guessesSoFarText);
+        losses++;
+
+        guessesSoFar.push(userChoice);
+        guessesSoFarText.textContent = guessesSoFar;
+        lossesText.textContent = losses;
+        console.log(guessesSoFar);
+
 
 
         //add any letters guessed so far to GuessedSoFar .push, display on screen guesssofar text to display. 
     }
 
+    
 
-
-}
+};
 
