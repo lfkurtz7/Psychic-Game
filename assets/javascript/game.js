7a9c85e1f15fdcf739d2f5542a10d9df386477e0
Leah Kurtz <lfkurtz7@gmail.com>
2019-10-17 16:49:55 -0400

    created onkeyup function, start function, and function to call random letter. Finished conditional statements and added some bootstrap

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,7 @@ var guessesSoFar = [];
 var computerChoice = ["a", "b", "c", "d", "e", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 console.log(computerChoice);
 
- 
+
 
 var userChoiceText = document.getElementById("userchoice-text");
 var computerChoiceText = document.getElementById("computerchoice-text"); // do i keep the same name for this variable?//
@@ -15,38 +15,47 @@ var lossesText = document.getElementById("losses-span");
 var guessesLeftText = document.getElementById("guessesLeft-span");
 var guessesSoFarText = document.getElementById("guessesSoFar-span");
 
-winsText.textContent= wins;
-lossesText.textContent= losses;
-guessesLeftText.textContent= guessesLeft;
-guessesSoFarText.textContent= guessesSoFar; 
-
+Start();
 
 document.onkeyup = function (event) {
     var userChoice = event.key;
-    var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)]; // showing up as undefined//
+    var computerGuess = getRandomIndex(computerChoice)
+    
+
 
     if (userChoice === computerGuess) {
         wins++
-
-        
-        winsText.textContent= wins;  
-        
-
-
-    } else {
-        losses++;
-
-        guessesSoFar.push(userChoice);
-        guessesSoFarText.textContent = guessesSoFar;
-        lossesText.textContent = losses;
-        console.log(guessesSoFar);
-
-
-
-        //add any letters guessed so far to GuessedSoFar .push, display on screen guesssofar text to display. 
+        Start();
     }
+    else if (guessesSoFar.includes(userChoice)) {
+        alert("You've already guessed this");
+    }
+    else {
+        guessesLeft--;
+        if (guessesLeft === 0) {
+            losses++;
+            Start();
+        }
 
-    
+        guessesSoFar.push(userChoice)
+        guessesLeftText.textContent = guessesLeft
+        guessesSoFarText.textContent = guessesSoFar;
 
+        console.log(guessesSoFar, computerGuess);
+
+    }
+};
+function Start() {
+    guessesLeft = 10;
+    guessesLeftText.textContent = guessesLeft;
+
+    guessesSoFar = [];
+    guessesSoFarText.textContent = guessesSoFar;
+
+    winsText.textContent = wins;
+    lossesText.textContent = losses;
+};
+function getRandomIndex(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
 };
 
